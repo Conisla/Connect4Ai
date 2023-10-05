588ef03586d93a65a09bc9381aa3740eeb6a4cd4
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-10-05 16:15:44 +0400

    Update server.js

--- Connect4Back/server.js
+++ Connect4Back/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {createBoard, isValidColumn, getOpenRow} = require('./connect4')
 
 const app = express();
 const httpServer = require("http").createServer(app);
@@ -8,15 +9,14 @@ const io = require("socket.io")(httpServer, {
     }
 });
 
-
 app.get('/', (req, res) => {
   res.send('<h1>Server ExpressJS</h1>');
 });
 
 io.on('connection', (socket) => {
-    socket.join("some room");
+
     // console.log('User connected');
-    console.log(`UserId : ${socket.id}`)
+    console.log(`UserId : ${socket.id} connected`)
     // console.log(`serveur : ${socket.rooms}`)
     
     socket.on('message', (data) => {
@@ -24,10 +24,35 @@ io.on('connection', (socket) => {
       io.emit('message', data);
 
       socket.emit('response', 'Message reçu !');
+      
     });
+    
+    socket.emit('startgame', (createBoard()));
+
+    socket.on('sendRow', (data) => {
+      // getOpenRow(board, column)
+    })
+
+    // // mouvement fait
+    // socket.on('makeMove', (move) => {
+    //   // joueur qui joue J1 = 1 J2 = 0
+    //   socket.on('player', (playerID) => {
+    //     socket.broadcast.emit('move:received', move)
+        
+    //     // joueur qui doit joué
+    //     socket.broadcast.emit('playermove', playerID)
+    //     console.log(playerID)
+    //   })
+    //   // console.log(`move: ${move}`)
+    // })
+
+    // // socket.on('gameOver', (gameOver) => {
+    // //   // socket.broadcast.emit('over', gameOver)
+    // //   console.log(gameOver)
+    // // })
 
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        console.log(`user ${socket.id} left`);
     });
 });
 
